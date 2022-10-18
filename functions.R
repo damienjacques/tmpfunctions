edf8508d9871c1f0c265826cf9c276913cfe417d
Damien Jacques <damienjacques1989@gmail.com>
2022-10-18 15:25:40 +0200

    fist commit

---
+++ functions.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+
+plotTimeSeriesTransaction <-
+  function(transaction_by_day,
+           date_field,
+           amount_field,
+           xlim,
+           cr_db_field) {
+    ggplot(transaction_by_day,
+           aes(x = date_field, y = amount_field)) +  geom_col(position = "identity", aes(fill = get(cr_db_field))) +
+      geom_col(position = "identity", width = 1) + theme_minimal() +  xlim(xlim) +
+      ylab("Transactions total amount") + xlab("Date") ++scale_fill_manual(values = c("darkgreen", "red"))
+    return(g)
+  }
